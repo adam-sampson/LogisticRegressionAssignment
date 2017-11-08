f54e120c24266887c929f048257f57640f19e3a2
Adam <12676475+adam-sampson@users.noreply.github.com>
2017-11-08 18:22:54 -0500

    Saved GeoKey in easier to use tab delimited format.

--- SampsonRegressionAnalysis.R
+++ SampsonRegressionAnalysis.R
@@ -1,5 +1,18 @@
+##########
+# Fall 2017, Bellarmine University MSA 
+# Logistic Regression and PCA Analysis Assignment
+# Student: Adam Sampson
+##########
+
 library(data.table)
 
 sol.dt <- fread("MarketingCampaignSolicitations.txt",sep="\t",header=TRUE)
 geo.dt <- fread("MarketingCampaignGeo+LandArea.txt",sep="\t",header=TRUE)
+geokey.dt <- fread("GeoKey.txt",sep="\t",header=TRUE)
 
+summary(sol.dt)
+  # MSRP from 300 to 6840
+  # OfferPrice from 60 to 70
+  # Two dates, Three Product Information Characters, and One record ID.
+summary(geo.dt)
+  # 
