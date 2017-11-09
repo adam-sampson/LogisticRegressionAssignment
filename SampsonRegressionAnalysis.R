9de2cf741ffc4ed58cb1deaafb3fef5da7d85ce7
Adam <12676475+adam-sampson@users.noreply.github.com>
2017-11-08 21:57:36 -0500

    Saving

--- SampsonRegressionAnalysis.R
+++ SampsonRegressionAnalysis.R
@@ -46,28 +46,15 @@ summary(geo.dt)
   geo.dt[ , names(geo.dt)[-1] := lapply(.SD,as.numeric), .SDcols = names(geo.dt)[-1]]
 
 # Convert census variables to percents
-    # geo.dt[1, c("ACSTTPOP0")] # = 1334 total population
-    # geo.dt[1, sum(.SD), .SDcols = c(4,6:14)] # = 1334 total people with ages 
-    # (skipping ACSAGE17YO because it is double-counting kids)
-  # ageVars <- c("ACSAGE4Y0",
-  #             "ACSAGE17Y0",
-  #             "ACSAGE59",
-  #             "ACSAGE1014",
-  #             "ACSAGE1519",
-  #             "ACSAGE2024",
-  #             "ACSAGE34Y0",
-  #             "ACSAGE44Y0",
-  #             "ACSAGE54Y0",
-  #             "ACSAGE64Y0",
-  #             "ACSAGE65Y0")
-    # geo.dt[1:10 , c(4,6:14)]
-  # geo.dt[ , (ageVars) := .SD / ACSTTPOP0, .SDcols = ageVars]
   # Columns 4: 14 are the age count columns
   geo.dt[ , names(geo.dt)[4:14] := .SD / ACSTTPOP0, .SDcols = names(geo.dt)[4:14]] 
     # geo.dt[1:10 , c(4,6:14)]
     # rm(ageVars)
-    
+  
+  # Calculate male percent based on formula, but then can't use same formula for female
   geo.dt[ , ACSMALES := ACSMALES / (ACSMALES + ACSFEMALES)]
   geo.dt[ , ACSFEMALES := 1 - ACSMALES]
   
-  geo.dt[ , names(geo.dt)[18:21]]
+  # Columns 18:21 are marital status
+  geo.dt[ , names(geo.dt)[18:21] := .SD / MARSTAT0, .SDcols = names(geo.dt)[18:21]]
+  
